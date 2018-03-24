81b494dfa91fdcbe2dceb523201b95950a353490
Tibor Leupold <Tibor@Caitlins-MacBook-Pro-2.local>
2018-03-24 19:11:35 +0100

    feat: Initial commit
    This already runs with the hard coded example files.

---
+++ pdfcombine.py
@@ -0,0 +1,28 @@
+from PyPDF2 import PdfFileWriter, PdfFileReader
+
+page_fronts_file = PdfFileReader('example/fronts123.pdf', 'rb')
+page_backs_file = PdfFileReader('example/backs321.pdf', 'rb')
+
+output = PdfFileWriter()
+
+# Get list of pages
+page_fronts = page_fronts_file.pages
+page_backs = page_backs_file.pages
+
+# Number of pages
+num_page_fronts = page_fronts_file.getNumPages()
+num_page_backs = page_backs_file.getNumPages()
+
+if num_page_backs != num_page_fronts:
+    raise ValueError
+else:
+    # Reverse page backs
+    page_backs = page_backs[::-1]
+    
+    # Merging
+    for i in range(0, page_fronts_file.getNumPages()):
+        output.addPage(page_fronts[i])
+        output.addPage(page_backs[i])
+    
+    with open('combined.pdf', 'wb') as f:
+        output.write(f)
