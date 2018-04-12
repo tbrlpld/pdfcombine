848cdd32696bd3e9384aff6162c111cb88d4bb86
tibsel <tibor.leupold@gmail.com>
2018-04-12 21:07:03 +0200

    feat: Updated file names of example files
    Also updated python and script path in the shell script

--- automator_pdfcombine.sh
+++ automator_pdfcombine.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-PYTHON="/Users/Tibor/.virtualenvs/pdf-combine-env/bin/python"
-SCRIPT_PATH="/Users/Tibor/dev/pdfcombine/pdfcombine.py"
-#echo "The arguments are:"
-#echo $1 
-#echo $2
+PYTHON="/Users/tibor/.virtualenvs/pdf-combine-env/bin/python"
+SCRIPT_PATH="/Users/tibor/dev/pdfcombine/pdfcombine.py"
+echo "The arguments are:"
+echo $1 
+echo $2
 
 $PYTHON $SCRIPT_PATH $1 $2
